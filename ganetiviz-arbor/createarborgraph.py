41b40b6282fc48ef40af53cd20df6d54c98cdbf3
Pranjal Mittal <mittal.pranjal@gmail.com>
2013-05-10 14:23:57 +0530

    Added code to give pnode-snode graph

--- ganetiviz-arbor/createarborgraph.py
+++ ganetiviz-arbor/createarborgraph.py
@@ -4,20 +4,44 @@
 # Pranjal Mittal
 #
 
+# Dictionary which maps nodes to list of primary nodes. 
 nodedict = {}
+
+# Dictionary which maps "instance groups" (primary_nodes) to a dictionary of secondary nodes also having weights.
+# Example : node1:{node3 : 5} => Means that 5 VMs having node1 as primary have node3 as secondary.
+# The dictionary is a Directed Graph.
+psdict = {}
+
 while True:
     s = raw_input()
     if s == '':
         break
     l = s.split()
+
+    ##Creating the PrimaryNode-Instance relations.
     try:
         nodedict[l[1]]
         nodedict[l[1]].append(l[0])
     except KeyError:
         nodedict[l[1]] = [l[0],]
 
+    ##Creating the "instance-group" to secondary node relations.
+    try:
+        # p[1] might not be already there in psdict, thats why we "try" it.
+        snodes = psdict[l[1]]
+        # Increase count of no. of links from pnode to the respective snode.
+        if l[2] in snodes:
+            snodes[l[2]] += 1
+        else:
+            if l[2] != 'None':
+                snodes[l[2]] = 1
+    #This exception occurs only when l[1] not in psdict.
+    except KeyError:
+        if l[2] != 'None':
+            psdict[l[1]] = {l[2]: 1}
 
-#from pprint import pprint as pp
+
+from pprint import pprint as pp
 #pp(nodedict)
 
 
@@ -40,3 +64,6 @@ for node in sorted(nodedict.keys()):
         print s
     print '\t},'
 
+
+pp(psdict)
+print
